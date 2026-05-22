fa7717223fc7710465d3b10fb1943660971c96c1
joao12345-junior <joaovpfarias@gmail.com>
2026-05-22 13:40:04 -0300

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,3 @@
-// next.config.mjs
-const nextConfig = {
-	trailingSlash: true,
-	serverExternalPackages: ["argon2"],
-	typescript: {
-		ignoreBuildErrors: false,
-	},
-	images: {
-		remotePatterns: [
-			{ protocol: "https", hostname: "*.com.br" },
-			{ protocol: "https", hostname: "*.org.br" },
-			{ protocol: "https", hostname: "storage.agil.net" },
-			{ protocol: "https", hostname: "imgbr.*.com" },
-			{ protocol: "https", hostname: "images.*.com" },
-			{ protocol: "https", hostname: "*.amazonaws.com" },
-			{ protocol: "https", hostname: "*.googleapis.com" },
-			{ protocol: "https", hostname: "*.googleusercontent.com" },
-			{ protocol: "https", hostname: "*.fbcdn.net" },
-			{ protocol: "https", hostname: "*.licdn.com" },
-			{ protocol: "https", hostname: "*.tripadvisor.com" },
-			{ protocol: "https", hostname: "*.ytimg.com" },
-			{ protocol: "https", hostname: "*.wixstatic.com" },
-			{ protocol: "https", hostname: "*.trvl-media.com" },
-		],
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
